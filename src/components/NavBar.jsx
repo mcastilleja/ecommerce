8b96cf9cb8888f4afc3f53461be813ebb739b8fb
Omar Soto <omarsotosainz@gmail.com>
2022-12-06 19:21:01 -0700

    Navbar - Header implementado.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,18 @@
 import { Link } from 'react-router-dom'
+import './navbar.scss'
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className='header'>
+      <Link to='/' className='header__logo'>LOGO</Link>
+      <ul className='header__nav-list'>
+        <li className='header__list-item'>
           <Link to='/'>Home</Link>
         </li>
-        <li>
+        <li className='header__list-item'>
           <Link to='/login'>Login</Link>
         </li>
-        <li>
+        <li className='header__list-item'>
           <Link to='/signup'>Signup</Link>
         </li>
       </ul>
