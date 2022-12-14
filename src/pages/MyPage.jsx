4c7f69eaa40a80275c6103a5b39cf322264b8083
Miguel Castilleja <castilleja.miguel@gmail.com>
2022-12-13 20:46:37 -0600

    feat: Implementada pagina personal

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
 const MyPage = () => {
+  const { user, userData } = useContext(AuthContext)
   return (
-    <div>MyPage</div>
+    <>
+      <div className='userData'>
+        <span>{user.role === 'CUSTOMER' ? 'Customer' : 'Admin'}</span>
+        <h1>Welcome {userData.first_name}</h1>
+        <h3>Your personal info:</h3>
+        <h5>Full Name: {userData.first_name} {userData.last_name}</h5>
+        <h5>Gender: {userData.gender === 'F' ? 'Female' : 'Male'} </h5>
+        <h5>Email: {userData.email}</h5>
+      </div>
+    </>
   )
 }
 
